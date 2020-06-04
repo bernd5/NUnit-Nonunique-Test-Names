99e535883450703a0bffb74a3566e975d2ec1384
Bernd Baumanns <baumannsbernd@gmail.com>
2020-06-04 22:02:18 +0200

    init

---
+++ TestSourcing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestCaseSourceJumpToGivenLocation
+{
+    public class TestSourcing
+    {
+        [TestCaseSource(nameof(GetTestFiles))]
+        public void TestHeader(FileInfo file)
+        {
+            //if (file.Name.Contains("TextFile1"))
+            //    Assert.Fail();
+            Assert.Pass();
+        }
+
+        public static IEnumerable<TestCaseData> GetTestFiles
+        {
+            get
+            {
+                var testFiles = Directory.GetFiles(TestContext.CurrentContext.WorkDirectory,"*.txt");
+                foreach (var f in testFiles)
+                {
+                    var td = new TestCaseData(new FileInfo(f))
+                        .SetDescription($"Description for: {f}")
+                        .SetSourceLocationEx(f);
+                    
+                    var fileName = Path.GetFileNameWithoutExtension(f).Replace(".", "_");
+                    td = td.SetCategory(fileName);
+                    td = td.SetName("TextFiles");
+
+                    td.SetProperty("_ID", fileName);
+                    yield return td;
+                        
+
+                    /* How to setup Jump-File here??? */
+                }
+            }
+        }
+    }
+}
